5f63915421e0946012cb0e794e18d7def12a62e0
bintadiaw423-sudo <bintadiaw423@gmail.com>
2026-04-07 14:42:52 +0000

    ajout fichier python avec fonctions

---
+++ app.py
@@ -0,0 +1,9 @@
+def saluer(nom):
+    return f"Bonjour {nom}, bienvenue dans mon projet Git !"
+
+def calculer(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(saluer("Binta"))
+    print("Somme :", calculer(5, 3))
